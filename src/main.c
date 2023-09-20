b6a514aecb37c15e8fac5f3b0a01316ad3c7b829
3x3cvte <3xecvte@gmail.com>
2023-09-20 11:58:14 +0300

    added server api, config

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
 #include "banner.h"
+#include "server_api.h"
 
 int main(void) 
 {
     printf("%s", BANNER_MAIN);
+
+    SERVER_API_init();
+    int bytes = SERVER_API_wait_message();
+    printf("bytes: %d", bytes);
+
     return 0;
 }
