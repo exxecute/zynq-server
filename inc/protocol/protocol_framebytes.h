34a07e5a6362141fc127dc04af289a05502d09fe
3x3cvte <3xecvte@gmail.com>
2023-09-22 11:40:43 +0300

    protocol: added package validation, package_type

--- inc/protocol/protocol_framebytes.h
+++ inc/protocol/protocol_framebytes.h
@@ -2,6 +2,7 @@
 #define PROTOCOL_FRAMEBYTES_H
 
 #include <stdint.h>
+#include "protocol.h"
 
 #define START_BYTE          (0xc0U)
 #define END_BYTE            (0xceU)
@@ -10,7 +11,7 @@ typedef struct PROTOCOL_start_head_t
 {
     uint8_t start;
     uint8_t package_size;
-    uint8_t code;
+    PROTOCOL_code_t code;
 }PROTOCOL_start_head_t;
 
 typedef struct PROTOCOL_end_head_t
