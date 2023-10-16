fa0052dcb1889837aa385c908faf795b2578f412
3x3cvte <3xecvte@gmail.com>
2023-10-16 10:45:47 +0300

    protocol: added structs for head protocols

---
+++ inc/protocol/protocol.h
@@ -0,0 +1,18 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include "package_api.h"
+
+typedef struct PROTOCOL_start_head_t
+{
+    struct PACKAGE_head_start_t start;
+    uint8_t command_code;
+    uint16_t package_size;
+}PROTOCOL_start_head_t;
+
+typedef struct PROTOCOL_stop_head_t
+{
+    struct PACKAGE_head_stop_t stop;
+}PROTOCOL_stop_head_t;
+
+#endif /* PROTOCOL_H */
