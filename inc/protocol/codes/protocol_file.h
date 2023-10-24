d04b78b642afcb4457e06b401839761de949702a
3x3cvte <3xecvte@gmail.com>
2023-10-24 09:58:54 +0300

    file protocol: add h and c processing structure

--- inc/protocol/codes/protocol_file.h
+++ inc/protocol/codes/protocol_file.h
@@ -1,3 +1,38 @@
 #ifndef PROTOCOL_FILE_H
 #define PROTOCOL_FILE_H
+
+#include "protocol.h"
+
+
+#define PROTOCOL_FILE_CODE          (0x02)
+
+#define PROTOCOL_FILE_CODE_ANSWER   (PROTOCOL_GET_ANSWER(PROTOCOL_FILE_CODE))
+#define PROTOCOL_FILE_CODE_REQUEST  (PROTOCOL_GET_REQUEST(PROTOCOL_FILE_CODE))
+
+#define PROTOCOL_FILE_PACKAGE_CODE  (PROTOCOL_FILE_CODE_REQUEST)
+#define PROTOCOL_FILE_PACKAGE_FOO   (PROTOCOL_FILE_process)
+
+#define PROTOCOL_FILE_DATA_LEN      (0xffff - (sizeof(PROTOCOL_start_head_t) + sizeof(PROTOCOL_stop_head_t)))
+
+
+#pragma pack(1)
+typedef struct PROTOCOL_FILE_request_t
+{
+    struct PROTOCOL_start_head_t start;
+    uint8_t file_code;
+    uint32_t file_offset;
+    struct PROTOCOL_stop_head_t stop;
+}PROTOCOL_FILE_request_t;
+
+typedef struct PROTOCOL_FILE_answer_t
+{
+    struct PROTOCOL_start_head_t start;
+    uint8_t file_data[PROTOCOL_FILE_DATA_LEN];
+    struct PROTOCOL_stop_head_t stop;
+}PROTOCOL_FILE_answer_t;
+
+
+uint16_t PROTOCOL_FILE_process(PROTOCOL_FILE_request_t* __buffer, PROTOCOL_FILE_answer_t* __answer_buffer);
+
+
 #endif /* PROTOCOL_FILE_H */
---
+++ src/protocol/codes/protocol_file.c
@@ -0,0 +1,31 @@
+#include "protocol_file.h"
+
+
+static uint16_t _optimize_answer(uint8_t *__answer_buffer)
+{
+    uint16_t _answer_size = 0;
+    return _answer_size;
+}
+
+static void _generate_answer(PROTOCOL_FILE_answer_t *this)
+{
+
+}
+
+static void _fill_answer_buffer(uint8_t *__answer_buffer, uint8_t __file_code, uint32_t __file_offset)
+{
+
+}
+
+uint16_t PROTOCOL_FILE_process(PROTOCOL_FILE_request_t* __buffer, PROTOCOL_FILE_answer_t* __answer_buffer)
+{
+    printf("[PROTOCOL] file package\n");
+
+    _generate_answer(__answer_buffer);
+    
+    _fill_answer_buffer(&__answer_buffer->file_data, __buffer->file_code, __buffer->file_offset);
+
+    uint16_t _answer_size = _optimize_answer((uint8_t*) __answer_buffer);
+
+    return _answer_size;
+}
